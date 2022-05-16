7b9b24f6db44c8960e2a3f8d1303443f2344d0cf
James Sgarella <Jimbo8702@gmail.com>
2022-05-16 13:54:07 -0400

    product and user models added and migrated

---
+++ admin/products/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# product class
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+    likes = models.PositiveBigIntegerField(default=8)
+
+
+# simple user with only id
+class User(models.Model):
+    pass
